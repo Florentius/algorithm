1a94ba320516fcd19713ebbf650c33ed85bf2e73
woojin <woojing.sim@samsung.com>
2019-06-11 19:56:16 +0900

    algorithm : quantize

--- quantize.cpp
+++ quantize.cpp
@@ -1,13 +1,47 @@
 #include <iostream>
-
+#include <algorithm>
+#include <cstring>
 using namespace std;
 
-int N,S;
-int V[100];
+#define INF 987654321;
 
+int N,S;
+int V[101];
+
+int cache[101][11];
+
+int min (int a, int b)
+{
+  if (a<b)
+    return a;
+  else
+    return b;
+}
+
+int minError(int lo, int hi)
+{
+  return 0;
+}
+int quantize(int from, int parts)
+{
+  if(from == N)
+    return 0;
+  if(parts ==0)
+    return INF;
+
+  int &ret = cache[from][parts];
+  if (ret != -1)
+    return ret;
+
+  ret = INF;
+  for(int partSize =1; from+partSize <= N; ++partSize)
+    {
+      ret = min(ret, minError(from, from+partSize-1) + quantize(from+partSize, parts-1);
+    }
+}
 int main()
 {
-  int text_case;
+  int test_case;
   cin >> test_case;
 
   if (test_case < 0 || test_case>50)
@@ -21,8 +55,10 @@ int main()
 	exit(-1);
       if(S>10 || S < 1)
 	exit(-1);
-      for(int j=0; j<N; j++)
+      for(int j=1; j<=N; j++)
 	cin >> V[j];
+
+      quantize(1, S);
     }
   return 0;
 
