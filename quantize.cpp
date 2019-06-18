23768c19c7896c507fdc7928e8205d2c9cc91203
woojin <woojing.sim@samsung.com>
2019-06-18 09:51:11 +0900

    algorithm : finish quantize algorithm
    algorithm# Please enter the commit message for your changes. Lines starting

--- quantize.cpp
+++ quantize.cpp
@@ -4,11 +4,20 @@
 using namespace std;
 
 #define INF 987654321;
+#define swap(a,b,c) (c=a, a=b, b=c)
 
 int N,S;
 
 int cache[101][11];
 int V[101], pSum[101], pSqSum[101];
+
+int min (int a, int b)
+{
+  if (a<b)
+    return a;
+  else
+    return b;
+}
 
 int partition(int arr[], int left, int right) {
 	int low,high,pivot,temp;
@@ -27,12 +36,6 @@ int partition(int arr[], int left, int right) {
 		if(low<high){
 			swap(arr[low],arr[high],temp);
 		}
-
-		printf("left =%d, right =%d, low =%d, high =%d\n", left, right, low, high);
-		for (int i=0; i<10; i++)
-			printf("%d ",arr[i]);
-		
-		printf("\n");
 	} while(low < high);
 
 	swap(arr[left], arr[high], temp);
@@ -44,7 +47,6 @@ void quick_sort(int arr[], int left, int right) {
 	int q;
 
 	if(left<right) {
-		printf("\nquick_sort, left %d right %d\n", left, right);
 		q = partition(arr, left, right);
 
 		quick_sort(arr, left, q-1);
@@ -52,18 +54,12 @@ void quick_sort(int arr[], int left, int right) {
 	}
 }
 
-int min (int a, int b)
-{
-  if (a<b)
-    return a;
-  else
-    return b;
-}
-
 void precalc() {
+  quick_sort(V, 0, N-1);
+			
   pSum[0] = V[0];
   pSqSum[0] = V[0] * V[0];
-  for(int i=1; i<n; i++)
+  for(int i=1; i<N; i++)
     {
       pSum[i] = pSum[i-1] + V[i];
       pSqSum[i] = pSqSum[i-1] + (V[i] * V[i]);
@@ -72,11 +68,11 @@ void precalc() {
 
 int minError(int lo, int hi)
 {
-  int sum = pSum[hi] - (lo == 0? 0 : pSum[lo-1]);
-  int sqSum = pSqSum[hi] - (lo == 0? 0 : pSqSum[lo-1]);
+  int sum = pSum[hi] - ((lo == 0)? 0 : pSum[lo-1]);
+  int sqSum = pSqSum[hi] - ((lo == 0)? 0 : pSqSum[lo-1]);
 
-  int m = int(0.5 + (double)sum / hi - lo + 1);
-  int ret = sqSum -2*m*sum + m*m*(hi-lo+1);
+  int m = int(0.5 + (double)sum / (hi - lo + 1));
+  int ret = sqSum - (2*m*sum) + (m*m*(hi-lo+1));
   return ret;
 }
 
@@ -95,8 +91,10 @@ int quantize(int from, int parts)
   ret = INF;
   for(int partSize =1; from+partSize <= N; ++partSize)
     {
-      ret = min(ret, minError(from, from+partSize-1) + quantize(from+partSize, parts-1);
+      ret = min(ret, minError(from, from+partSize-1) + quantize(from+partSize, parts-1));
     }
+
+  return ret;
 }
   
 int main()
@@ -112,13 +110,15 @@ int main()
       memset(cache, -1, sizeof(cache));
       cin >> N >> S;
       if(N > 100 || N < 1)
-	exit(-1);
+		exit(-1);
       if(S>10 || S < 1)
-	exit(-1);
+		exit(-1);
       for(int j=0; j<N; j++)
-	cin >> V[j];
+		cin >> V[j];
 
-      quantize(0, S);
+	  precalc();
+	  cout << quantize(0, S) << endl;
+	  
     }
   return 0;
 
